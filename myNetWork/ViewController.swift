30ee9a9a7d027f42d396abdc693351135db017fe
Larry Hung <a85857766a@yahoo.com.tw>
2019-08-10 14:47:08 +0800

    Initial Commit

---
+++ myNetWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myNetWork
+//
+//  Created by 洪得維 on 2019/8/10.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
